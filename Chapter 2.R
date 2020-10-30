55c37a345cca7ee6693719e0c347a90d66d67f8d
Brandon <al159905@alumnos.uacj.mx>
2020-10-29 23:46:38 -0600

    R Tutorial chapter 2

---
+++ Chapter 2.R
@@ -0,0 +1,46 @@
+# R arithmetic
+4^4
+28 %% 6
+6 + 6
+12 - 6
+5 * 5
+(6+6)/3
+5^34
+2^5
+
+#Variable assingment
+
+myVar <- 8
+print(myVar)
+
+#variable assignment (2)
+
+my_apples <- 8
+my_apples
+
+#variable assignment (3)
+
+my_apples <- 6
+myVar
+my_oranges
+my_
+my_fruit <- my_apples + my_oranges
+
+#Apples and oranges
+
+my_apples <- 6
+my_oranges <- 6 #Error in my_apples + my_oranges : argumento no-numérico para operador binario
+print(my_fruit <- my_apples + my_oranges)
+
+#Basic data types in R
+
+myNumeric <- 4.5 #numeric value
+myInteger <- 4 #integer value
+myBool <- FALSE #logical value
+myCharacter <- "character value"
+
+#What's that data type?
+
+class(myBool)
+class(myNumeric)
+class(myInteger)
